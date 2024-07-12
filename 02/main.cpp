407da3d57529bd1534df686d9b8d0429929470ea
Denis Smal <smoladen2003@gmail.com>
2024-07-12 18:16:01 +0200

    Added right version of 02/main.cpp

--- 02/main.cpp
+++ 02/main.cpp
@@ -26,6 +26,59 @@ public:
         digids = std::move(other.digids);
         return *this;
     }
+
+    big_integer operator+(const big_integer& other) const {
+        std::string result;
+        int carry = 0;
+        size_t maxLength = std::max(this->digids.length(), other.digids.length());
+
+        for (size_t i = 0; i < maxLength; ++i) {
+            int digit1 = (i < this->digids.length()) ? this->digids[i] - '0' : 0;
+            int digit2 = (i < other.digids.length()) ? other.digids[i] - '0' : 0;
+            int sum = digit1 + digit2 + carry;
+            result.push_back((sum % 10) + '0');
+            carry = sum / 10;
+        }
+
+        if (carry) {
+            result.push_back(carry + '0');
+        }
+
+
+        std::reverse(result.begin(), result.end());
+        return big_integer(result);
+    }
+
+
+    big_integer operator*(const big_integer& other) const {
+        std::string result(this->digids.length() + other.digids.length(), '0');
+
+        for (size_t i = 0; i < this->digids.length(); ++i) {
+            int carry = 0;
+            for (size_t j = 0; j < other.digids.length(); ++j) {
+                int temp = (result[i + j] - '0') + (this->digids[i] - '0') * (other.digids[j] - '0') + carry;
+                result[i + j] = (temp % 10) + '0';
+                carry = temp / 10;
+            }
+            result[i + other.digids.length()] += carry;
+        }
+
+
+        while (result.length() > 1 && result.back() == '0') {
+            result.pop_back();
+        }
+
+        std::reverse(result.begin(), result.end());
+        return big_integer(result);
+    }
+
+
+    friend std::ostream& operator<<(std::ostream& os, const big_integer& bi) {
+        std::string numberReversed = bi.digids;
+        std::reverse(numberReversed.begin(), numberReversed.end());
+        os << numberReversed;
+        return os;
+    }
 };
 
 
